0a2045aa5211d737c6f961f0d60a5526977cf17a
nubnuengSitthichok <59160040@go.buu.ac.th>
2019-10-19 17:11:49 +0700

    Get data into the adapte

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -74,6 +74,16 @@ class SleepTrackerFragment : Fragment() {
         binding.setLifecycleOwner(this)
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
+
+
+
         // Add an Observer on the state variable for showing a Snackbar message
         // when the CLEAR button is pressed.
         sleepTrackerViewModel.showSnackBarEvent.observe(this, Observer {
